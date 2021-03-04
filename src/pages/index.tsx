04116bea7ed020f341e9f1929df1c52f6305938f
Jhon9191 <joaoantonio15161516@gmail.com>
2021-03-04 17:35:24 -0300

    Configuring texts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,28 @@ export default function Home() {
       </Head>
 
       <Header/>
-
+      
       <main className={styles.main}>
         <div className={styles.body}>
+
+          <div className={styles.textBoasVindas}>
+            <h1>Seja bem vindo!</h1>
+          </div>
+
         <Photo/>
+
+            
+              <div className={styles.history}>
+                  <p>
+                  Olá meu nome é <b>João Antônio</b>, tenho 21 anos de idade,  sou programador
+                  <b> FULL STACK</b> iniciante mas já com bastante prrojetos praticados e postados
+                  no github, eu amo codificar e amo ainda mais o javascript, pois ele foi
+                  o meu primeiro ponto que eu fiquei agarrado, mas depois de estudar e estudar,
+                  vi o quanto ele é poderoso para criação de API, sites, aplicativos,etc.
+                  </p>
+              </div>
+            
+
 
         </div>
       </main>
