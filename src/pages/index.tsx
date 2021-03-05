e4ff1542e8880408b8a040db47179860f850328a
Jhon9191 <joaoantonio15161516@gmail.com>
2021-03-04 21:18:21 -0300

    Working on the responsiveness of the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,29 +12,45 @@ export default function Home() {
         <title>João Antônio</title>
       </Head>
 
-      <Header/>
-      
-      <main className={styles.main}>
-        <div className={styles.body}>
+      <Header />
 
+      <main className={styles.main}>
           <div className={styles.textBoasVindas}>
             <h1>Seja bem vindo!</h1>
+            <h4>Hora de codar</h4>
           </div>
 
-        <Photo/>
+        <div className={styles.body}>
+         <img className={styles.imagem} src="fotoJoao.jpg" alt="Foto-João" />
+        <div className={styles.card}>
+        <p>
+                Olá meu nome é <b>João Antônio</b>, tenho 21 anos de idade,  sou programador
+                <b> FULL STACK</b> iniciante mas já possuo bastante projetos postados
+                no github, eu amo codificar e amo ainda mais o javascript, pois ele foi
+                o meu primeiro ponto que eu fiquei agarrado, mas depois de estudar e estudar,
+                vi o quanto ele é poderoso para criação de API, sites, aplicativos,etc.
+                </p>
+        </div>
 
-            
-              <div className={styles.history}>
-                  <p>
-                  Olá meu nome é <b>João Antônio</b>, tenho 21 anos de idade,  sou programador
-                  <b> FULL STACK</b> iniciante mas já com bastante prrojetos praticados e postados
-                  no github, eu amo codificar e amo ainda mais o javascript, pois ele foi
-                  o meu primeiro ponto que eu fiquei agarrado, mas depois de estudar e estudar,
-                  vi o quanto ele é poderoso para criação de API, sites, aplicativos,etc.
-                  </p>
+         
+
+          {/* <div  className={styles.containerCard}>
+            <div className={styles.subCard} style={{margin: 25}}>
+              <div className={styles.marcador}></div>
+              <div className={styles.subCard} style={{
+                marginTop: 30, marginInlineStart: 30, padding: 20,
+                background: "#fff", color: "#000", justifyContent: 'center',
+                alignItems: 'center', display: 'flex'}}>
+                   <p>
+                Olá meu nome é <b>João Antônio</b>, tenho 21 anos de idade,  sou programador
+                <b> FULL STACK</b> iniciante mas já possuo bastante projetos postados
+                no github, eu amo codificar e amo ainda mais o javascript, pois ele foi
+                o meu primeiro ponto que eu fiquei agarrado, mas depois de estudar e estudar,
+                vi o quanto ele é poderoso para criação de API, sites, aplicativos,etc.
+                </p>
               </div>
-            
-
+            </div>
+          </div> */}
 
         </div>
       </main>
